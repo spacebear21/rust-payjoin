89a596038c4344ff98cc9a77967dec0a82ff4ad6
user <benalleng@gmail.com>
2025-06-19 12:13:20 -0400

    Specify the language to create bindings at the bindings creation step
    I've included support for including --language args beyond those explicitly
    supported in uniffi-rs with a match block that passes the chosen language
    to chose what binding cargo package to use.

--- payjoin-ffi/uniffi-bindgen.rs
+++ payjoin-ffi/uniffi-bindgen.rs
@@ -1,13 +1,39 @@
+use std::env;
+
 fn main() {
-    #[cfg(feature = "uniffi")]
-    uniffi::uniffi_bindgen_main();
-    #[cfg(feature = "uniffi")]
-    uniffi_dart::gen::generate_dart_bindings(
-        "src/payjoin_ffi.udl".into(),
-        None,
-        Some("dart/lib".into()),
-        "target/release/libpayjoin_ffi.dylib".into(),
-        true,
-    )
-    .unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    let language =
+        args.iter().position(|arg| arg == "--language").and_then(|idx| args.get(idx + 1));
+
+    match language {
+        Some(lang) if lang == "dart" => {
+            #[cfg(feature = "uniffi")]
+            uniffi_dart::gen::generate_dart_bindings(
+                "src/payjoin_ffi.udl".into(),
+                None,
+                Some(
+                    args.iter()
+                        .position(|arg| arg == "--out-dir")
+                        .and_then(|idx| args.get(idx + 1))
+                        .expect("No output directory found")
+                        .as_str()
+                        .into(),
+                ),
+                args.iter()
+                    .position(|arg| arg == "--library")
+                    .and_then(|idx| args.get(idx + 1))
+                    .expect("No target file found")
+                    .as_str()
+                    .into(),
+                true,
+            )
+            .expect("Failed to generate dart bindings");
+        }
+        Some(lang) if lang == "python" => {
+            #[cfg(feature = "uniffi")]
+            uniffi::uniffi_bindgen_main();
+        }
+        _ => panic!("No language specified"),
+    }
 }
